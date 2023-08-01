2f3a0d0bf1e3440f1fac2be632125e652635f9f2
nicolasgsantana <nicolas07san@gmail.com>
2023-08-01 20:34:22 -0300

    Add paint and change cell count functions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
+let fillColor = "blue";
+let cells;
+
+const board = document.getElementById('board');
+const button = document.getElementById('change-cell-count');
+
 function createGrid(cellCount) {
+
+    if(board.hasChildNodes()){
+        Array.from(board.childNodes).forEach(row => {
+            Array.from(row.childNodes).forEach(cell => {
+                cell.remove();
+            });
+            row.remove();
+        });
+        cells = [];
+    }
+
     for(let i = 0; i < cellCount; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
@@ -12,8 +29,27 @@ function createGrid(cellCount) {
         
         board.appendChild(row);
     }
+
+    cells = Array.from(document.getElementsByClassName('cell'));
+    cells.forEach(cell => {
+    cell.addEventListener('mouseover', () => paintCell(cell, fillColor));
+    });
 }
 
-const board = document.getElementById('board');
+function paintCell(cell, color){
+    cell.style.backgroundColor = color;
+}
+
+button.addEventListener('click', () => {
+    cellCount = parseInt(prompt("Enter the new value (Max: 100)"));
+    if(cellCount > 100){
+        createGrid(100);
+    }
+    else {
+        createGrid(cellCount);
+    }
+})
 
 createGrid(16);
+
+
