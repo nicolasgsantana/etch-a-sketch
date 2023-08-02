1460be093f0b0e404937e0354ede91c726571544
nicolasgsantana <nicolas07san@gmail.com>
2023-08-02 15:45:58 -0300

    Add grid toggle

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ let cells;
 
 const board = document.getElementById('board');
 const button = document.getElementById('change-cell-count');
-const colorInput = document.getElementById('color');
 
-function createGrid(cellCount) {
+const colorInput = document.getElementById('color');
+const gridCheckbox = document.getElementById('grid');
+
+function createBoard(cellCount) {
 
     if(board.hasChildNodes()){
         Array.from(board.childNodes).forEach(row => {
@@ -44,15 +46,25 @@ function paintCell(cell, color){
 button.addEventListener('click', () => {
     cellCount = parseInt(prompt("Enter the new value (Max: 100)"));
     if(cellCount > 100){
-        createGrid(100);
+        createBoard(100);
     }
     else {
-        createGrid(cellCount);
+        createBoard(cellCount);
     }
 })
 
 colorInput.addEventListener('input',  () => fillColor = colorInput.value);
 
-createGrid(16);
+gridCheckbox.addEventListener('input', () => {
+    board.classList.toggle('grid');
+    Array.from(board.childNodes).forEach(row => {
+        Array.from(row.childNodes).forEach(cell => {
+            cell.classList.toggle('grid');
+        });
+        row.classList.toggle('grid');
+    }); 
+});
+
+createBoard(16);
 
 
